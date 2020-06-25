c0f1614d73ad3bb84131d63a13ba54a9211d8923
Crowdbotics <team@crowdbotics.com>
2020-06-25 06:41:44 +0000

    Update models

--- backend/vehicle/api/v1/urls.py
+++ backend/vehicle/api/v1/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import VehicleViewSet, VehicleTypeViewSet
 
 router = DefaultRouter()
+router.register("vehicletype", VehicleTypeViewSet)
+router.register("vehicle", VehicleViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
